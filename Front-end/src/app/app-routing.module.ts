131e08afaca9332a2cf2086cb0ccccbc5e7501a5
Navinda Senavirathna <151707959+NavindaSenavirathna@users.noreply.github.com>
2025-05-22 14:39:24 +0530

    Add routing and component declarations for book management

--- Front-end/src/app/app-routing.module.ts
+++ Front-end/src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BookListComponent } from './components/book-list/book-list.component';
+import { BookDetailsComponent } from './components/book-details/book-details.component';
+import { BookFormComponent } from './components/book-form/book-form.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/books', pathMatch: 'full' },
+  { path: 'books', component: BookListComponent },
+  { path: 'details/:id', component: BookDetailsComponent },
+  { path: 'add', component: BookFormComponent },
+  { path: 'edit/:id', component: BookFormComponent },
+  { path: '**', redirectTo: '/books' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- Front-end/src/app/app.module.ts
+++ Front-end/src/app/app.module.ts
@@ -5,11 +5,16 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BookListComponent } from './components/book-list/book-list.component';
+import { BookDetailsComponent } from './components/book-details/book-details.component';
+import { BookFormComponent } from './components/book-form/book-form.component';
 
-@NgModule({
-  declarations: [
-    AppComponent
-  ],  imports: [
+@NgModule({  declarations: [
+    AppComponent,
+    BookListComponent,
+    BookDetailsComponent,
+    BookFormComponent
+  ],imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
