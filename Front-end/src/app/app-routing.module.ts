cdf7fa65abb1171c335ae1f60e8ebb1e6c5683e0
Navinda Senavirathna <151707959+NavindaSenavirathna@users.noreply.github.com>
2025-05-22 19:29:22 +0530

    Enhance navigation and layout for improved UI/UX

--- Front-end/src/app/app-routing.module.ts
+++ Front-end/src/app/app-routing.module.ts
@@ -3,14 +3,15 @@ import { RouterModule, Routes } from '@angular/router';
 import { BookListComponent } from './components/book-list/book-list.component';
 import { BookDetailsComponent } from './components/book-details/book-details.component';
 import { BookFormComponent } from './components/book-form/book-form.component';
+import { HomeComponent } from './components/home/home.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: '/books', pathMatch: 'full' },
+  { path: '', component: HomeComponent },
   { path: 'books', component: BookListComponent },
   { path: 'details/:id', component: BookDetailsComponent },
   { path: 'add', component: BookFormComponent },
   { path: 'edit/:id', component: BookFormComponent },
-  { path: '**', redirectTo: '/books' }
+  { path: '**', redirectTo: '/' }
 ];
 
 @NgModule({
